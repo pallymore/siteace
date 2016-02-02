45943944abef9d416bf5bdee03baa6a42e9e8b9d
Yurui Zhang <eraclaymore@hotmail.com>
2016-02-01 20:30:24 -0500

    update ratign button functionality

--- client/websites/rating_buttons/rating_buttons.js
+++ client/websites/rating_buttons/rating_buttons.js
@@ -1,19 +1,3 @@
-function userUpvote(user_id ,website_id) {
-  return Votes.findOne({
-    user_id: user_id,
-    website_id: website_id,
-    upvote: true
-  });
-}
-
-function userDownvote(user_id ,website_id) {
-  return Votes.findOne({
-    user_id: user_id,
-    website_id: website_id,
-    upvote: false
-  });
-}
-
 function setErrorMessage(template, message) {
   template.errorMessage.set(message);
 }
@@ -22,60 +6,63 @@ Template.rating_buttons.created = function () {
   this.errorMessage = new ReactiveVar(null);
 };
 
-// TODO: simplify! user can only have one vote! (remove or update!)
 // TODO: display color by vote type
+
+function handleVote(user_id, website_id, isUpvote) {
+  var existingVote = Votes.findOne({ website_id: website_id, user_id: user_id });
+
+  if(!existingVote) {
+    Votes.insert({
+      website_id: website_id,
+      user_id: user_id,
+      upvote: isUpvote,
+      createdOn: new Date()
+    });
+    return false;
+  }
+
+  if(existingVote.upvote !== isUpvote) {
+    setErrorMessage(this, 'Your vote has been updated.');
+    Votes.update({
+      _id: existingVote._id
+    }, { $set: {
+      upvote: isUpvote,
+      createdOn: new Date()
+    }});
+    return false;
+  } else {
+    Votes.remove({ _id: existingVote._id });
+  }
+
+  return false; // prevent the button from reloading the page
+}
 
 Template.rating_buttons.events({
   "click .js-upvote" : function (event, template){
     var website_id = this._id,
       currentUser = Meteor.user();
 
-    if (currentUser) {
-      var existingUpvote = userUpvote(currentUser._id, website_id);
-      if(!existingUpvote) {
-        var downvote = userDownvote(currentUser._id, this._id);
-        if (downvote) { Votes.remove({ _id: downvote._id }); }
-        Votes.insert({
-          website_id: this._id,
-          user_id: currentUser._id,
-          upvote: true,
-          createdOn: new Date()
-        });
-      } else {
-        Votes.remove({ _id: existingUpvote._id });
-      }
-      setErrorMessage(template, null);
-    } else {
+    setErrorMessage(template, null);
+
+    if (!currentUser) {
       setErrorMessage(template, 'Please login to vote');
+      return false;
     }
 
-    return false; // prevent the button from reloading the page
+    return handleVote.call(template, currentUser._id, website_id, true);
   },
   "click .js-downvote": function (event, template){
     var website_id = this._id,
       currentUser = Meteor.user();
 
-    if (currentUser) {
-      var existingDownvote = userDownvote(currentUser._id, website_id);
-      if(!existingDownvote) {
-        var upvote = userUpvote(currentUser._id, this._id);
-        if (upvote) { Votes.remove({ _id: upvote._id }); }
-        Votes.insert({
-          website_id: this._id,
-          user_id: currentUser._id,
-          upvote: false,
-          createdOn: new Date()
-        });
-      } else {
-        Votes.remove({ _id: existingDownvote._id });
-      }
-      setErrorMessage(template, null);
-    } else {
+    setErrorMessage(template, null);
+
+    if (!currentUser) {
       setErrorMessage(template, 'Please login to vote');
+      return false;
     }
 
-
-    return false; // prevent the button from reloading the page
+    return handleVote.call(template, currentUser._id, website_id, false);
   }
 });
 
